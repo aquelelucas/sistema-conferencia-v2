0d8cd86f1ce7d0380ea18abc13c40286410616e8
aquelelucas <serra.lucas042@gmail.com>
2026-09-18 13:19:12 -0300

    Atualiza endpoint para nova implantação da API de conferência

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbwlJAOQ8sPFkADagsvCfENQ5CilWGr9TsShXrw1fk5M_LlJA4wGj3yr9l32Pxlhg7c5tQ/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzmUBStoR7Y5LR52A8vV-yGAMGFcomsKXGDRoDmuvGcxtoK6Q9o5WGxyUR7Due3mUG22g/exec';
 
 let sessao = null;
 let nomeConferente = null;
